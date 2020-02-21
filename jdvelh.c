1832ca85f988e7c40025d2d6bc9a2244311d8028
DournauxNathan <dournauxnathan@gmail.com>
2020-02-21 14:40:48 +0100

    Update Magasin

--- jdvelh.c
+++ jdvelh.c
@@ -2,9 +2,43 @@
 #include <string.h>
 #include <stdio.h>
 
+/*Structure des articles du magasin referencant leur nom et leurs prix*/
+struct Objet{
+	char nomObjet[30];
+	int prixObjet;
+};
+typedef struct Objet objet;
+
 int main()
 {
 	int choixJoueur; 
+	/*Systeme_Nnventaire & Shop*/
+		int orJoueur = 50;
+
+		/*Compteur d'objet*/
+		int nPomme = 0;
+		int nEau = 0;
+		int nPotionRouge = 0;
+		int nPotionVerte = 0;
+		int nEpee = 0;
+		int nBouclier = 0;
+
+		/*Liste des articles proposés dans le magasin*/
+			//Consommable
+			objet pomme = {"Pomme",5};
+			objet eau =  {"Eau",2};
+			objet potionRouge = {"Potion rouge",5};
+			objet potionVerte = {"Potion vert",10};
+			//Armes
+			objet epee =  {"Epee en bois",15};
+			//Armures
+			objet bouclier = {"Bouclier en bois", 25};
+
+		/*Pointeur redirigant vers le nom de l'objet*/
+		void Achat(objet * monObjet)
+		{
+			orJoueur = orJoueur - (*monObjet).prixObjet;
+		}
 
 
 
@@ -30,21 +64,53 @@ int main()
 	printf("Choix 3\n");
 	printf("......................\n\n");
 
-	scanf("%d", &choixJoueur); printf("......................\n);
+	scanf("%d", &choixJoueur); printf("\n\n");
 
 	switch (choixJoueur)
-		{
-			case 1 :
-				printf("Chapitre 2 \n\n");
-			break;
+	{
+		case 1 :
+			printf("_________________\n\n");
+			printf("Magasin - Chapitre 2 \n\n");
+			printf("_________________\n\n");
+			/*Choix du menu: Magasin, Inventaire et Quittez*/
+			printf("BlaBlaBlaBla\n\n");
+			printf("Que vendez-vous ? [1]  Inventaire [2]  Quittez [3] \n\n");
+			printf("-- --\n");
+			printf("Votre choix: "); scanf("%d", &choixJoueur);
+			printf("-- --\n\n");
 
-			case 2 :
-				printf("Chapitre 3 \n\n");
-			break;
+			switch (choixJoueur) 
+			{
+				case 1 :
+					printf("Marchand: Des babioles, des petites choses...\n\n");
+					printf("_____________________\n\n");
+					printf("Or: %d\n\n", orJoueur);
 
-			case 3 :
-				printf("Chapitre 4 \n\n");
-			break;
+					//Appel de la fonction Achat
+					//Select();
+				break;
 
-		}
+				case 2 :
+					//Inventaire();
+				break;
+
+				case 3 :
+					printf("Marchand: A bientot !\n\n");
+					return 1;
+				break;
+			}
+
+		break;
+
+		case 2 :
+			printf("Bandits - Chapitre 3 \n\n");
+		break;
+
+		case 3 :
+			printf("Auberge - Chapitre 4 \n\n");
+		break;
+
+	}
+
+	return 0;
 }
