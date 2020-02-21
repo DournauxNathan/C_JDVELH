1860abefa7639215c37343c2a2bbdea04e81fe7d
DournauxNathan <dournauxnathan@gmail.com>
2020-02-21 12:14:38 +0100

    Voyage Chapitres
    Permet au joueur de choisir vers quel chapitres il veut allez

--- jdvelh.c
+++ jdvelh.c
@@ -4,5 +4,47 @@
 
 int main()
 {
-	
+	int choixJoueur; 
+
+
+
+
+	/*Début de l'aventure textuel*/
+	printf("\n");
+	printf("...............\nReveille du personnages\n..............\n\n");
+
+	printf("...............\nBlabla quetes\n..............\n\n");
+
+	printf("...............\nDebut de l'aventure\n........\n\n");
+
+
+	printf("_________________\n\n");
+	printf ("CHAPITRE 1 - DEBUT DE L'AVENTURE\n"); 
+	printf("_________________\n\n");
+
+	printf("Blabl a homme - choix de dialgoue\n");
+
+	printf("......................\n");
+	printf("Choix 1\n");
+	printf("Choix 2\n");
+	printf("Choix 3\n");
+	printf("......................\n\n");
+
+	scanf("%d", &choixJoueur); printf("......................\n);
+
+	switch (choixJoueur)
+		{
+			case 1 :
+				printf("Chapitre 2 \n\n");
+			break;
+
+			case 2 :
+				printf("Chapitre 3 \n\n");
+			break;
+
+			case 3 :
+				printf("Chapitre 4 \n\n");
+			break;
+
+		}
 }
